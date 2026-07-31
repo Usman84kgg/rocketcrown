50383a669b65346facd7b053e9f2aa7c41ea8acc
Usman84kgg <usmanbinancep2p@gmail.com>
2026-07-31 04:54:26 +0300

    Create Header.tsx

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <div className="flex items-center justify-between p-3 bg-black/80 border-b border-purple-500/20">
+      <span className="text-pink-500 font-bold text-lg">ROCKET</span>
+      <span className="text-purple-500 font-bold text-lg">CROWN</span>
+    </div>
+  );
+}
